8b10e1df4ef04c56bba0955221c41da9d7b9520c
Дейнеко Максим <madeyneko@gmail.com>
2022-01-27 14:00:29 +0800

    Init class GeoCode

--- GeoCode.py
+++ GeoCode.py
@@ -1,10 +1,13 @@
-def getGeoCode(address):   #Получаем геопозиции города
-    global API
-    url = 'https://geocode-maps.yandex.ru/1.x?geocode="'+address+'"&apikey='+API+'&format=json'
-    resp = requests.get(url)
-    resp = resp.json()['response']['GeoObjectCollection']['featureMember']
-    if len(resp) > 0:
-        return resp[0]['GeoObject']['boundedBy']['Envelope']
-    else:
-        print('NOT FOUND! --> ' + address)
-        return '- -'
+class GeoCode:
+    def __init__(self, api):
+        self.API=api
+
+    def getGeoCode(self, address):   #Получаем геопозиции города
+        url = 'https://geocode-maps.yandex.ru/1.x?geocode="'+address+'"&apikey='+self.API+'&format=json'
+        resp = requests.get(url)
+        resp = resp.json()['response']['GeoObjectCollection']['featureMember']
+        if len(resp) > 0:
+            return resp[0]['GeoObject']['boundedBy']['Envelope']
+        else:
+            print('NOT FOUND! --> ' + address)
+            return '- -'
