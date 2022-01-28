4db9b04085b4ffcd50f8d519c94336f059978c74
MADeyneko <madeyneko@gmail.com>
2022-01-28 14:50:10 +0800

    Init Us

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ from bs4 import BeautifulSoup
 from lxml import html
 import urllib
 
+class Us:
+    def __init__(self, path):
+        self.path=path
+        self.pd=pd.read_csv(path, sep=';')
+        print(self.pd.head())
+
+    def getUniqueAddress(self):
+        self.pd['city_street']=self.pd['CITY']+' '+ self.pd['street']+' '+self.pd['found_house_number']
+        return pd.unique(self.pd['city_street'])
+
 if __name__=='__main__':
-    g=geo.GeoCode(conf.API)
-    g.getGeo('колобок центральная')
+    us=Us('us.csv')
+    unique_add=us.getUniqueAddress()
+    print(unique_add)
+    # g=geo.GeoCode(conf.API)
+    # g.getGeo('колобок центральная')
