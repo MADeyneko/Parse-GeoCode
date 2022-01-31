5aab903af5bd1dcc7ca262a3da119bf4e9d8c931
MADeyneko <madeyneko@gmail.com>
2022-01-31 19:33:08 +0800

    Added GeoAddress

--- main.py
+++ main.py
@@ -12,6 +12,57 @@ from bs4 import BeautifulSoup
 from lxml import html
 import urllib
 
+class PBar:
+    def __init__(self, desc:str, size: int):
+        self.widgets=[
+            desc
+            , progressbar.Bar(left='', marker='▓', right='')
+            , progressbar.SimpleProgress()
+            , ' [',progressbar.Timer(), '] '
+        ]
+        self.bar=progressbar.ProgressBar(
+            maxval=size,
+            widgets=self.widgets).start()
+
+class GeoAddress:
+
+    def __init__(self):
+        file=open('address_geocode', 'r+')
+        self.add=self.get_address(file.read().splitlines())
+        self.file=file
+        self.list=[]
+        self.geo=geo.GeoCode(conf.API)
+
+    def get_address(self, rows):
+        c=[]
+        for value in rows:
+            c.append(value.split(';')[0])
+
+        return c
+
+    def add_adress(self, list):
+        self.bar=PBar('Added address:', len(list)).bar
+        count=0
+        try:
+            for value in list[1:-1]:
+                if (value in self.add)==False:
+                    g=self.geo.getGeo(value)
+                    self.add.append(value)
+                    self.list.append(value+';'+g['lowerCorner']+';'+g['upperCorner'])
+                count+=1
+                self.bar.update(count)
+        except :
+            self.file_save()
+            self.file.close()
+
+    def file_save(self):
+        for row in self.list:
+            self.file.write("%s\n" % row)
+        print("Save file \n")
+
+    def __del__(self):
+        self.file.close()
+
 class Us:
     def __init__(self, path):
         self.path=path
@@ -24,10 +75,6 @@ class Us:
 
 if __name__=='__main__':
     us=Us('us.csv')
-    g=geo.GeoCode(conf.API)
     unique_add=us.getUniqueAddress()
-    # print(unique_add)
-    for add in unique_add[1:10]:
-        print(add)
-        print(g.getGeo(add))
-    # g.getGeo('колобок центральная')
+    ga=GeoAddress()
+    ga.add_adress(unique_add)
